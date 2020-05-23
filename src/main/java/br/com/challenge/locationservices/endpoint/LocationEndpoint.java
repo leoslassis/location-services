b809ee88f66a87d6a9656d3ca02d93897a92bdb9
Leonardo da Silva Lima Assis <lsassis@uolinc.com>
2020-05-23 00:09:02 -0300

    adicionando swagger

--- src/main/java/br/com/challenge/locationservices/endpoint/LocationEndpoint.java
+++ src/main/java/br/com/challenge/locationservices/endpoint/LocationEndpoint.java
@@ -3,6 +3,8 @@ package br.com.challenge.locationservices.endpoint;
 import br.com.challenge.locationservices.endpoint.resource.LocationResource;
 import br.com.challenge.locationservices.service.LocationService;
 import br.com.challenge.locationservices.service.LocationServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "Location API REST")
 @RestController
 @RequestMapping("/rs/location")
 public class LocationEndpoint {
@@ -17,6 +20,7 @@ public class LocationEndpoint {
     @Autowired
     private LocationService locationService;
 
+    @ApiOperation(value = "Buscar localização através do CEP")
     @GetMapping
     public ResponseEntity<LocationResource> findByPostalCode(
             @RequestParam(name = "postalCode", required = true) String postalCode){
